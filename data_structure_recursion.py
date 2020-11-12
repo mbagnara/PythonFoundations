857de28cf46d9dabcc53b7bf7c5965aaa838c429
mariobagnara <mbagnara@gmail.com>
2020-11-11 20:02:36 -0700

    Modified factorial and iterative examples to make them simpler

--- data_structure_recursion.py
+++ data_structure_recursion.py
@@ -7,7 +7,7 @@ def my_func(n):
     return my_func(n)
 
 
-print(my_func(3))
+# print(my_func(3))
 
 # Calculates factorial
 def factorial_iterative(num):
@@ -18,15 +18,33 @@ def factorial_iterative(num):
     if num <= 2:
         return num
 
-    # starts at 2
+    # Since the iteration starts at 3, acc_prev starts with the resultado returned by the number 2 that is 2
     acc_prev = 2
-    x = 0
 
+    # Range starts at 3, since numbers 0, 1 and 3 and special cases 0->1, 1->1 and 2->2
     for current_number in range(3, num + 1):
-        x = current_number * acc_prev
-        acc_prev = x
+        acc_prev = acc_prev * current_number
 
     return acc_prev
 
 
-print(factorial_iterative(6))
+print("Iterative", factorial_iterative(6))
+
+
+def factorial_recursion(num):
+
+    # base case
+    if num == 0:
+        return 1
+
+    # base case
+    if num <= 2:
+        return num
+
+    if num == 0:
+        return 'Done'
+
+    return num * factorial_recursion(num - 1)
+
+
+print("Recursive", factorial_recursion(6))
